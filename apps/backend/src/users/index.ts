49cac361511e57fab45ece2f512b026b448e14c4
Thanh Phong Le <ltphongssvn@gmail.com>
2026-02-19 17:17:37 -0800

    feat: add users barrel export

---
+++ apps/backend/src/users/index.ts
@@ -0,0 +1,4 @@
+// apps/backend/src/users/index.ts
+export { UsersModule } from './users.module';
+export { UsersService } from './users.service';
+export { UsersController } from './users.controller';
