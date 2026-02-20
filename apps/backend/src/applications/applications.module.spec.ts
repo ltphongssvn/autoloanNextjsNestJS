d758822de219ba2b9a4abce7a22536420960e252
Thanh Phong Le <ltphongssvn@gmail.com>
2026-02-19 18:45:39 -0800

    test: update ApplicationsModule spec with StatusHistoryService

--- apps/backend/src/applications/applications.module.spec.ts
+++ apps/backend/src/applications/applications.module.spec.ts
@@ -3,25 +3,13 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ApplicationsModule } from './applications.module';
 import { ApplicationsController } from './applications.controller';
 import { ApplicationsService } from './applications.service';
+import { StatusHistoryService } from './status-history.service';
 
 describe('ApplicationsModule', () => {
   let module: TestingModule;
-
-  beforeEach(async () => {
-    module = await Test.createTestingModule({
-      imports: [ApplicationsModule],
-    }).compile();
-  });
-
-  it('should compile the module', () => {
-    expect(module).toBeDefined();
-  });
-
-  it('should provide ApplicationsController', () => {
-    expect(module.get<ApplicationsController>(ApplicationsController)).toBeDefined();
-  });
-
-  it('should provide ApplicationsService', () => {
-    expect(module.get<ApplicationsService>(ApplicationsService)).toBeDefined();
-  });
+  beforeEach(async () => { module = await Test.createTestingModule({ imports: [ApplicationsModule] }).compile(); });
+  it('should compile', () => expect(module).toBeDefined());
+  it('should provide ApplicationsController', () => expect(module.get<ApplicationsController>(ApplicationsController)).toBeDefined());
+  it('should provide ApplicationsService', () => expect(module.get<ApplicationsService>(ApplicationsService)).toBeDefined());
+  it('should provide StatusHistoryService', () => expect(module.get<StatusHistoryService>(StatusHistoryService)).toBeDefined());
 });
