230493a0a1632718eff81907532f829ca629ff80
Thanh Phong Le <ltphongssvn@gmail.com>
2026-02-19 16:01:20 -0800

    feat: add applications barrel export

---
+++ apps/backend/src/applications/index.ts
@@ -0,0 +1,5 @@
+// apps/backend/src/applications/index.ts
+export { ApplicationsModule } from './applications.module';
+export { ApplicationsService } from './applications.service';
+export { ApplicationsController } from './applications.controller';
+export { CreateApplicationDto } from './create-application.dto';
