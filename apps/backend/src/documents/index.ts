5c23f18916ccba62a0622d4a3ecab0e3cc7202e8
Thanh Phong Le <ltphongssvn@gmail.com>
2026-02-19 17:10:24 -0800

    feat: add documents barrel export

---
+++ apps/backend/src/documents/index.ts
@@ -0,0 +1,4 @@
+// apps/backend/src/documents/index.ts
+export { DocumentsModule } from './documents.module';
+export { DocumentsService } from './documents.service';
+export { DocumentsController } from './documents.controller';
