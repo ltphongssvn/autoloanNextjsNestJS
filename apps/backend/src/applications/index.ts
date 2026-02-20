1ecaa09ca02148959f687f1e54224bb1d36f838a
Thanh Phong Le <ltphongssvn@gmail.com>
2026-02-19 18:26:56 -0800

    feat: add StatusHistoryService to applications barrel export

--- apps/backend/src/applications/index.ts
+++ apps/backend/src/applications/index.ts
@@ -3,3 +3,4 @@ export { ApplicationsModule } from './applications.module';
 export { ApplicationsService } from './applications.service';
 export { ApplicationsController } from './applications.controller';
 export { CreateApplicationDto } from './create-application.dto';
+export { StatusHistoryService } from './status-history.service';
