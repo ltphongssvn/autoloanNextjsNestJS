65ca85291927ad9235812aabb33d2699977093c2
Thanh Phong Le <ltphongssvn@gmail.com>
2026-02-19 17:12:14 -0800

    feat: add notes barrel export

---
+++ apps/backend/src/notes/index.ts
@@ -0,0 +1,4 @@
+// apps/backend/src/notes/index.ts
+export { NotesModule } from './notes.module';
+export { NotesService } from './notes.service';
+export { NotesController } from './notes.controller';
